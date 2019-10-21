2b8f8f3cf7561264f3d35aaf46728a531ae2a56c
Yegun Kim <wlfak102@gmail.com>
2019-10-22 08:46:01 +0900

    분대 그리드뷰 구현

---
+++ app/src/main/java/com/example/makeus/Module/SquadButton.java
@@ -0,0 +1,18 @@
+package com.example.makeus.Module;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatButton;
+
+import com.example.makeus.Model.Squad;
+
+public class SquadButton extends AppCompatButton {
+
+    public Squad squad;
+
+    public SquadButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+}
--- app/src/main/java/com/example/makeus/ViewModel/SquadViewModel.java
+++ app/src/main/java/com/example/makeus/ViewModel/SquadViewModel.java
@@ -1,17 +1,30 @@
 package com.example.makeus.ViewModel;
 
-import androidx.databinding.ObservableList;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.makeus.Model.Squad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SquadViewModel extends ViewModel {
     // TODO: Implement the ViewModel
-    private ObservableList<Squad> Squads;
+    private MutableLiveData<List<Squad>> mSquads;
 
     public SquadViewModel() {
-
+        ArrayList<Squad> squads = new ArrayList<>();
+        squads.add(new Squad("1분대"));
+        squads.add(new Squad("2분대"));
+        squads.add(new Squad("3분대"));
+        squads.add(new Squad("4분대"));
+        squads.add(new Squad("5분대"));
+        mSquads = new MutableLiveData<>();
+        mSquads.setValue(squads);
     }
 
+    public MutableLiveData<List<Squad>> getSquads() {
+        return mSquads;
+    }
 }
