b869f2c4e19caf18ebd4d3d41cf8130eba11c404
DaeSeok_Song <zxdsaqwe1231@naver.com>
2019-10-22 08:52:50 +0900

    1. Solider 클래스 기반 작성, Squad 클래스 작성 중 2. Rank, Specialty 클래스 추가

--- app/src/main/java/com/example/makeus/Model/Soldier.java
+++ app/src/main/java/com/example/makeus/Model/Soldier.java
@@ -2,8 +2,26 @@ package com.example.makeus.Model;
 
 public class Soldier {
     public String Name;
+    public String Squad;
+    public Rank Rank;
+    private String Millitary_Number;
+    private long Birthday;
+    private long Enlistment_Day;
+    private long Transfer_Day;
+    private long Discharge_Day;
+    private boolean Discharge_Flag;
+    private Specialty Specialty;
 
-    public Soldier(String name) {
+    public void Input_Infomation(String name, String squad, String millitary_Number, long birthday, long enlistment_Day, long transfer_Day, long discharge_Day, boolean discharge_Flag, Rank rank, Specialty specialty){
         this.Name = name;
+        this.Squad = squad;
+        this.Millitary_Number = millitary_Number;
+        this.Birthday = birthday;
+        this.Enlistment_Day = enlistment_Day;
+        this.Transfer_Day = transfer_Day;
+        this.Discharge_Day = discharge_Day;
+        this.Discharge_Flag = discharge_Flag;
+        this.Rank = rank;
+        this.Specialty = specialty;
     }
 }
---
+++ app/src/main/java/com/example/makeus/Model/Specialty.java
@@ -0,0 +1,7 @@
+package com.example.makeus.Model;
+
+public class Specialty {
+    public String Name;
+
+    public Specialty(String name){ Name = name; }
+}
