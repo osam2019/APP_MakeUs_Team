f5ec8e80b7e3da908cf9d8a64b4855e70ffa69da
Kim Hyeoncheol <effectmix@gmail.com>
2019-10-22 09:50:22 +0900

    SoliderFragment 추가

---
+++ app/src/main/java/com/example/makeus/ViewModel/SoliderFragment.java
@@ -0,0 +1,38 @@
+package com.example.makeus.ViewModel;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.makeus.R;
+
+public class SoliderFragment extends Fragment {
+
+    private SoliderViewModel mViewModel;
+
+    public static SoliderFragment newInstance() {
+        return new SoliderFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.solider_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(SoliderViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/makeus/ViewModel/SoliderViewModel.java
@@ -0,0 +1,9 @@
+package com.example.makeus.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class SoliderViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+
+
+}
