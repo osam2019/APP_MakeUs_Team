ef5a2e78b4545197d5fcaf6364d6a73ab0af348b
Kim Hyeoncheol <effectmix@gmail.com>
2019-10-23 20:39:24 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/makeus/Module/PhysicalTestAdapter.java
    #	app/src/main/java/com/example/makeus/ViewModel/Adapter/PhysicalTestAdapter.java
    #	app/src/main/java/com/example/makeus/ViewModel/Adapter/PhysicalmanageAdapter.java
    #	app/src/main/java/com/example/makeus/ViewModel/Fragment/PhysicalTestFragment.java

--- app/src/main/java/com/example/makeus/Module/DateCalculator.java
+++ app/src/main/java/com/example/makeus/Module/DateCalculator.java
@@ -1,5 +1,7 @@
 package com.example.makeus.Module;
 
+import android.util.Log;
+
 import com.example.makeus.Model.Soldier;
 
 import java.text.ParseException;
@@ -10,7 +12,73 @@ import java.util.Date;
 import java.util.Locale;
 
 public class DateCalculator {
+
+        final String DISCHARGE = "DISCHARGE";
+
     public void getDischargeDay(long enlistment_day) throws ParseException {
+        // 1. 단축이전 군복무 일수구하기
+        // 입대한날짜 + (21개월 * 30일 = 630)
+        // 입대한달을 따로(less than or equal to 30일)
+        // + 임대한달 이후 31일 달의 갯수 => 총 20달 체크  31일인 달: 1, 3, 5, 7, 8, 10, 12
+        // + (30일 - 입대한달의 복무수)
+        // + 그해가 윤달인가? 윤달이면 -1 아니면 -2
+        // 2. 입대한달이 2017-01-03 > x  인경우 복무단축일자 적용 x?         -   적용 x
+        // 3. 입대한달이 2017-01-03 <= x <= 2020-06-15 인가?                 - 1일 ~ 90일
+        // 4. 입대한달이 2020-06-16 부터인가?                                -   3개월
+        //
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyy-MMM-dd", Locale.KOREA);
+        Date elistmentDate = new Date(enlistment_day);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(elistmentDate);
+        int year = cal.get(Calendar.YEAR);
+        int month = (cal.get(Calendar.MONTH) + 1);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int restFromEnlistmentDay = 0;
+
+        // 입대한 달에 복무가능한 날 갯수 계산
+        if(isMonthwith31days(month)) {
+            restFromEnlistmentDay = (31 - day) + 1;
+        } else if( month == 2 ) {
+            if(isLeapMonth(year)) {
+                restFromEnlistmentDay = (29 - day) + 1;
+            } else {
+                restFromEnlistmentDay = (29 - day) + 1;
+            }
+        } else {
+            restFromEnlistmentDay = (30 - day) + 1;
+        }
+
+        // 입대한달 이후 20개월동안 31일 달과 28일 혹은 29일의 총합 연산
+        int additionalDay = 0;
+
+        for(int i=0; i<20; i++) {
+            // 달 업데이트 이후, 달이 다음해로 지내는지 체크
+            month = ((month + 1) % 12);
+
+            // 다음달이 1월이면 year 업데이트
+            if(month == 1) {
+                year++;
+            }
+
+            if(isMonthwith31days(month)) {
+                additionalDay++;
+            } else if( month == 2 ) {
+                if(isLeapMonth(year)) {
+                    additionalDay -= 1;
+                } else {
+                    additionalDay -= 2;
+                }
+            }
+        }
+
+        //
+
+
+        // ----------------------------------------------------------------------------------------
+        /*
         SimpleDateFormat formatter_one = new SimpleDateFormat ( "yyyyMMddhh",Locale.KOREA );
         SimpleDateFormat formatter_two = new SimpleDateFormat ( "yyyy-MM-dd" );
         SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
@@ -24,8 +92,17 @@ public class DateCalculator {
 
         long dDay = b.getTime() - d.getTime();
         long diffdays = dDay / (24 * 60 * 60 *1000);
-        int i = (int)(diffdays / 14) + 1;
-        int discharge = 640 - i;
+
+        int i = 0;
+
+        if((int)(diffdays%14) != 0) {
+            i = (int)(diffdays / 14) + 1;       // suscipicious point
+        } else {
+            i = (int)(diffdays / 14);
+        }
+
+        int discharge = 640 - i;    // difficult to define the whole period is 640 days
+
         System.out.println("2: " +discharge);
 
         Calendar cal = Calendar.getInstance();
@@ -43,9 +120,17 @@ public class DateCalculator {
         cal.add(Calendar.DAY_OF_MONTH, discharge);
         int nDay = cal.get(Calendar.DAY_OF_MONTH);
         int bonus = 0;
-        if (((double) discharge / 30) - (discharge / 30) == 0.0) bonus = 0;
-        else if (((double) discharge / 30) > 18.6) bonus = 1;
-        else bonus = 2;
+
+        if (((double) discharge / 30) - (discharge / 30) == 0.0) {
+            bonus = 0;
+        }
+        else if (((double) discharge / 30) > 18.6) {
+            bonus = 1;
+        }
+        else {
+            bonus = 2;
+        }
+
         for (int k = 1; k < discharge / 30 + bonus; k++) {
             if(tmpMonth == 2){
                 if (year_flag == 0) {
@@ -116,50 +201,44 @@ public class DateCalculator {
         sbDate.append(nDay);
 
         System.out.println(sbDate);
+        */
+
     }
 
-    public void getPersonalityTestDay(long transfer_day, String rank){
-        Calendar cal = Calendar.getInstance();
-        int m = cal.get(Calendar.MONTH) + 1;
-        int y = cal.get(Calendar.YEAR);
-
-        if(rank == "이병" || rank == "일병") {
-            /*SimpleDateFormat formatter_one = new SimpleDateFormat ( "yyyyMMddhh",Locale.KOREA );
-            SimpleDateFormat formatter_two = new SimpleDateFormat ( "yyyy-MM-dd" );
-            SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
-
-            ParsePosition pos = new ParsePosition (0);
-            Date frmTime = formatter_one.parse (Long.toString(enlistment_day), pos );
-            String outString = formatter_two.format (frmTime);
-            Date d = formatter_two.parse("2017-01-03");
-            Date b = formatter_two.parse(outString);
-            System.out.println("1: " +outString);
-
-            long dDay = b.getTime() - d.getTime();
-            long diffdays = dDay / (24 * 60 * 60 *1000);
-            int i = (int)(diffdays / 14) + 1;
-            int discharge = 640 - i;
-            */
+    private boolean isMonthwith31days(int month) {
+        // 1, 3, 5, 7, 8, 10, 12
+        if(month == 0 || month == 1 || month == 3 || month == 5
+        || month == 7 || month == 8 || month == 10 || month == 12) {
+            return true;
+        } else {
+            return false;
         }
     }
 
-    public void getHeathTestDay(Soldier soldier) {
-        /*
-        SimpleDateFormat formatter_one = new SimpleDateFormat ( "yyyyMMddhh",Locale.KOREA );
-        SimpleDateFormat formatter_two = new SimpleDateFormat ( "yyyy-MM-dd" );
-        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
+    private boolean isLeapMonth(int year) {
+        if(year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-        ParsePosition pos = new ParsePosition (0);
-        Date frmTime = formatter_one.parse (Long.toString(enlistment_day), pos );
-        String outString = formatter_two.format (frmTime);
-        Date d = formatter_two.parse("2017-01-03");
-        Date b = formatter_two.parse(outString);
-        System.out.println("1: " +outString);
+    public void getPersonalityTestDay(long transfer_day, String rank){
+        Calendar curCal = Calendar.getInstance();
+        int month = curCal.get(Calendar.MONTH) + 1;
+        int year = curCal.get(Calendar.YEAR);
 
-        long dDay = b.getTime() - d.getTime();
-        long diffdays = dDay / (24 * 60 * 60 *1000);
-        int i = (int)(diffdays / 14) + 1;
-        int discharge = 640 - i;
-         */
+        if(rank.equals("이병") || rank.equals("일병")) {
+            // Compare transfer_day with current date
+            Calendar transferCal = Calendar.getInstance();
+            transferCal.setTime(new Date(transfer_day));
+
+            int yearDiff = curCal.get(Calendar.YEAR) -
+
+        }
+    }
+
+    public void getHealthScreeningDay(Soldier soldier) {
+
     }
 }
--- app/src/main/java/com/example/makeus/ViewModel/Adapter/PhysicalmanageAdapter.java
+++ app/src/main/java/com/example/makeus/ViewModel/Adapter/PhysicalmanageAdapter.java
@@ -17,12 +17,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class PhysicalmanageAdapter extends BaseAdapter {
+public class PhysicalTestAdapter extends BaseAdapter {
 
     private final Context mContext;
     private List<Soldier> soldiers;
 
-    public PhysicalmanageAdapter(Context context, List<Soldier> soldiers) {
+    public PhysicalTestAdapter(Context context, List<Soldier> soldiers) {
         this.mContext = context;
         this.soldiers = soldiers;
     }
@@ -47,6 +47,7 @@ public class PhysicalmanageAdapter extends BaseAdapter {
 
         if (convertView == null) {
             final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.physicalManageFragment, null);
         }
 
         Soldier soldier = soldiers.get(position);
@@ -68,6 +69,14 @@ public class PhysicalmanageAdapter extends BaseAdapter {
         TextView physicalRunning = convertView.findViewById(R.id.in_physic_running);
         SimpleDateFormat dateFormat = new SimpleDateFormat("mm:ss");
         physicalRunning.setText( dateFormat.format(new Date(physicalScore.getRunning())) );
+
+        LinearLayout viewPhysicalScoreLayout = convertView.findViewById(R.id.physical_test_layout);
+        viewPhysicalScoreLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Open Dialog for Pnysical score", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return convertView;
     }
