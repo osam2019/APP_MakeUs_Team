8098e9d2f70b1f60e8209e947d9092256245ef24
DaeSeok_Song <zxdsaqwe1231@naver.com>
2019-10-23 12:16:15 +0900

    1. DateCalc 거의 완성

--- app/src/main/java/com/example/makeus/MainActivity.java
+++ app/src/main/java/com/example/makeus/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.makeus;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.makeus.Module.DateCalculator;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.text.ParseException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DateCalculator d = new DateCalculator();
+        try {
+            d.getDischargeDay(2019111100);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/example/makeus/Model/Test.java
@@ -0,0 +1,7 @@
+package com.example.makeus.Model;
+
+public class Test {
+    public boolean pt;
+    public boolean st;
+    public boolean df;
+}
